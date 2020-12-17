dcce75ad0f3d35b8a26b3d80e3edee661b232d81
Doreen <doreen.jakob@gmx.de>
2020-12-17 12:47:53 +0100

    Stopwörter implementiert

--- chatbot.py
+++ chatbot.py
@@ -9,10 +9,15 @@
 
 import random
 
-zufallsantworten = ["Oh wirklich...", "Interessant",
-                    "Das kann man so sehen.", "Ich verstehe..."]
+# zufallsantworten = ["Oh wirklich...", "Interessant", "Das kann man so sehen.", "Ich verstehe..."]
 # ELIZA: eigtl. in externer Datei speichern und dann in main
 # einem initialisierten Array zuweisen
+
+# Stopwörter
+reaktionen = {"hallo": "aber hallo",
+              "geht": "Was verstehst du darunter",
+              "schmeckt": "Ich habe keinen Geschmackssinn"
+              }
 
 print("Willkommen beim ChatBot (v1)")
 print("Worüber wollen Sie sprechen?")
@@ -23,5 +28,12 @@ nutzereingabe = ""  # sauberer Stil: Variablentyp initialisieren
 while nutzereingabe != "bye":
     nutzereingabe = ""  # Variable säubern: nichts bleibt übrig
     nutzereingabe = input("Ihre Frage oder Antwort: ")
-    print(random.choice(zufallsantworten))  # Nachteil an Laufvariable (zufallsantworten[i] (mit i = random.randint(1,4) )): ist auf Länge der zufallsanworten angewiesen
+    nutzereingabe = nutzereingabe.lower()
+    nutzerwoerter = nutzereingabe.split()
+    
+    for einzelwoerter in nutzerwoerter:
+        if einzelwoerter in reaktionen:
+            print(reaktionen[einzelwoerter])
+
+    # print(random.choice(zufallsantworten))  # Nachteil an Laufvariable (zufallsantworten[i] (mit i = random.randint(1,4) )): ist auf Länge der zufallsanworten angewiesen
 print("Einen schönen Tag.")
