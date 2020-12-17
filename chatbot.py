4a15356d57e672039431e8add17f8affd3f49cca
Doreen <doreen.jakob@gmx.de>
2020-12-17 11:12:01 +0100

    Initialer Commit

---
+++ chatbot.py
@@ -0,0 +1,17 @@
+#!//usr/bin/env python3
+# -*- oding: utf-8 -*-
+
+# auf allen Systemen wird die passende Environment für Python 3
+# automat. gewählt
+
+# Ein einfacher ChatBot
+# (c) 2020 by me, Lizenz GPLv3
+
+print("Willkommen beim ChatBot (v1)")
+print("Worüber wollen Sie sprechen?")
+print("Zum Beenden geben Sie bye ein....")
+print("")
+
+nutzereingabe = ""  # sauberer Stil: Variablentyp initialisieren
+nutzereingabe = input("Ihre Frage oder Antwort: ")
+print(nutzereingabe)
